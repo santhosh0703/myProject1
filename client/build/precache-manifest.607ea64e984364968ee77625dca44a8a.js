e9694fdff2a4c46859d7e6b481aeebc9deb292f6
santhosh0703 <santhoshrama007@gmail.com>
2020-11-21 12:27:34 +0530

    updated

---
+++ client/build/precache-manifest.607ea64e984364968ee77625dca44a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d3b6d0b295cb7ecc2a3ffe9932bc2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d952a9e87a1428f93e82",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9925967913e0ee5a26dc",
+    "url": "/static/js/2.d9cb8a21.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "/static/js/2.d9cb8a21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d952a9e87a1428f93e82",
+    "url": "/static/js/main.b5c3bc9d.chunk.js"
+  },
+  {
+    "revision": "65326020070271871721",
+    "url": "/static/js/runtime-main.2df13cd9.js"
+  }
+]);
