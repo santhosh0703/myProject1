1bc0ac1d6d78babf7e921206c8a05fd7bc210d30
santhosh0703 <santhoshrama007@gmail.com>
2020-11-21 12:45:02 +0530

    PORT updated

--- server/node_react_forum.js
+++ server/node_react_forum.js
@@ -28,5 +28,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
