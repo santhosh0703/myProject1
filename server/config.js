8c7ad540c7665d1e318acf70208bd76286cd882c
SANTHOSH RAMALINGAM T <santhoshrama007@gmail.com>
2026-07-20 21:12:21 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoUri: "mongodb+srv://santhosh0703:qweqwe@cluster0.8t8zb.mongodb.net/klenty?retryWrites=true&w=majority"
+    mongoUri: "mongodb+srv://santhosh0703:test@cluster0.8t8zb.mongodb.net/klenty?retryWrites=true&w=majority"
 }
